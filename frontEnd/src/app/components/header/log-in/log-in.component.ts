68e5b91c1718e5950927cb3f95358d961aea7887
Alexey-Kleschov <iron-goga@yandex.ru>
2019-04-26 00:27:59 +0300

    Adding authorization

--- frontEnd/src/app/components/header/log-in/log-in.component.ts
+++ frontEnd/src/app/components/header/log-in/log-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, TemplateRef, Output, EventEmitter } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 
@@ -17,53 +17,66 @@ export class LogInComponent implements OnInit {
 
   ngOnInit() {
   }
-  response: any;
-  server(){
-  
-   this.http.get('http://localhost:3000/check').subscribe((response)=>{
-     this.response = response;
-     console.log(this.response);
-    });
-    
+
+  enterData = {
+    name: 'admin',
+    logInOut: false,
+    enterQuit: 'Log In',
+    modalLogIn: true,
+    enterState: false,
+    isLogin: false
   }
 
+//Check login and logining
+  logining(loginValue, passwordValue){
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      })
+    };  
+    
+    let user = {
+      login: loginValue,
+      password: passwordValue
+    };
+    
+    this.http.post('http://localhost:3000/login', user, httpOptions).subscribe((data: any) => {
+      console.log(data); 
+      this.enterData.isLogin = data.isLogin; 
+      if(this.enterData.isLogin){
+        this.enterData.logInOut = true;
+        this.enterData.modalLogIn = false;
+        this.enterData.enterState = true;
+        this.enterData.enterQuit = 'Quit';
+        this.onSetName.emit(this.enterData); 
+      } else alert('Wrong password or e-mail');        
+    });  
+      
+        
+         
+  }
+//--------------------------------------
+
+checkIsLogin(){
+  if(this.enterData.enterState){
+    let qa = function() { return confirm('Do you really want to quit?'); }     
+    if(qa()){
+     this.enterData.logInOut = false;
+     this.enterData.modalLogIn = true;
+     this.enterData.enterQuit = 'Log In';
+     this.enterData.enterState = false;
+     this.onSetName.emit(''); 
+    }
+   }
+}
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
  
   @Output() onSetName: EventEmitter<any> = new EventEmitter(); 
   
-  enterData = {
-    name: 'admin',
-    logInOut: false,
-    enterQuit: 'Log In',
-    modalLogIn: true,
-    enterState: false
-  }
-
-  setName(login, password){
-    if(!this.enterData.enterState){
-      if(login=='admin' && password==491956){
-        this.enterData.logInOut = true;
-        this.enterData.modalLogIn = false;
-        this.enterData.enterState = true;
-        this.enterData.enterQuit = 'Quit';
-        this.onSetName.emit(this.enterData);       
-      } else alert('Wrong password or e-mail');
-    }     
-  }
-
-  checkEnterState(){
-    if(this.enterData.enterState){
-     let qa = function() { return confirm('Do you really want to quit?'); }     
-     if(qa()){
-      this.enterData.logInOut = false;
-      this.enterData.modalLogIn = true;
-      this.enterData.enterQuit = 'Log In';
-      this.enterData.enterState = false;
-      this.onSetName.emit(''); 
-     }
-    }
-  }
+  
 
 }
