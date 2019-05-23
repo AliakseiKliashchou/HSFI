c402f36c66c4e03ec59c640e49145667c7e7ceb4
Alexey-Kleschov <iron-goga@yandex.ru>
2019-05-24 01:23:08 +0300

    23/05/19

--- backEnd/routes/report_router.js
+++ backEnd/routes/report_router.js
@@ -6,7 +6,7 @@ const VendorCard = require('../models/vendorCard');
 
 const reportRouter = express.Router();
 
-reportRouter.post('/report', async(req, res, next) => {     
+reportRouter.post('/report', (req, res, next) => {     
    var findingData = {
        0: '',
        1: '',
@@ -16,19 +16,163 @@ reportRouter.post('/report', async(req, res, next) => {
        5: '',
        6: '',
        7: ''
-   };
-     for(let i = 0; i < 8; i++){
-//Main logic. Here is proccess incoming data from client side.        
-       if(req.body[i] == 'Registered vendors'){
-        Vendor.find({}, (err, doc) => {
-            if(err){console.log(err);}
-            findingData[i] = doc.length;           
+   };  
+//----------------------------------TASK 0: Finding all registered vendors and return length of array--------------------------------------
+    function task_0(){
+        return new Promise((resolve) => {
+                   
+                if(req.body[0] == 'Registered vendors'){
+                    Vendor.find({}, (err, doc) => {
+                        if(err){console.log(err);}
+                        findingData[0] = doc.length;                                                   
+                        resolve();     
+                    });                          
+                }else {resolve();}                      
+                              
         });
-       }
-       
     }
-    //console.log(findingData); 
-    await res.json(findingData);
+//----------------------------------TASK 1: Finding vendors in food groups--------------------------------------
+    function task_1(){
+        return new Promise((resolve) => {
+            let counter = {
+                meat: 0,
+                fastfood: 0,
+                fruit: 0,
+            };
+                    
+                if(req.body[1] == 'Vendors by group'){
+                 Vendor.find({}, (err, doc) => {
+                    if(err){console.log(err);}
+                    for(let i = 0; i < doc.length; i++){
+                        if(doc[i].foodGroup == 'Meat'){counter.meat++;}
+                        if(doc[i].foodGroup == 'Fastfood'){counter.fastfood++;}
+                        if(doc[i].foodGroup == 'Fruit'){counter.fruit++;}                        
+                    }
+                    findingData[1] = counter;                   
+                    resolve();     
+                 });                          
+                } else{resolve();}                         
+                        
+        });
+    }
+//----------------------------------TASK 2: Middle OSS value--------------------------------------
+    function task_2(){
+      return new Promise((resolve) => {
+         
+            if(req.body[2] == 'Average OSS'){
+                let counter = 0;
+                let count = 0;          
+              Vendor.find({}, (err, doc) => {
+                  if(err){console.log(err);}
+                  for(let i = 0; i < doc.length; i++){
+                      count+=doc[i].oss;
+                  }                  
+                  counter = parseFloat((count/doc.length).toFixed(2));
+                  findingData[2] = counter;                  
+                  resolve();     
+               });    
+            }else{resolve();} 
+          
+      });  
+    }
+//----------------------------------TASK 3: Quantity of 'red flags'--------------------------------------
+    function task_3(){
+        return new Promise((resolve) => {
+            
+                if(req.body[3] == 'Total red flags'){
+                    let counter = 0;
+                    Vendor.find({}, (err, doc) =>{
+                        if(err){console.log(err);}
+                        for(let i = 0; i < doc.length; i++){
+                            if(doc[i].flag == 'redFlagged'){
+                                counter++;
+                            }
+                        }
+                        findingData[3] = counter;                        
+                        resolve();      
+                    });
+                }else {resolve();} 
+            
+        });
+    }    
+//----------------------------------TASK 4: Average of stars--------------------------------------
+    function task_4(){
+        return new Promise((resolve) => {
+            
+                if(req.body[4] == 'Average quality stars'){
+                    let counter = 0;
+                    Vendor.find({}, (err, doc) => {
+                        if(err){console.log(err);}                        
+                        for(let i = 0; i < doc.length; i++){
+                            counter += doc[i].stars;
+                        }
+                        findingData[4] = counter;                        
+                        resolve();
+                    });
+                }else{resolve();} 
+            
+        });
+    }
+//----------------------------------TASK 5: Quantity of hotline calls--------------------------------------
+    function task_5(){
+        return new Promise((resolve) => {
+            
+                if(req.body[5] == 'Hotline calls'){
+                    let counter = 0;
+                    Vendor.find({}, (err, doc) => {
+                        if(err){console.log(err);}                        
+                        for(let i = 0; i < doc.length; i++){
+                            counter += doc[i].callCount;
+                        }
+                        findingData[5] = counter;                        
+                        resolve();
+                    });
+                }else {resolve();} 
+            
+        });
+    }
+//----------------------------------TASK 6: Map transactions--------------------------------------
+
+//----------------------------------TASK 7: Total revenues--------------------------------------
+    function task_7(){
+        return new Promise((resolve) => {
+            
+                if(req.body[7] == 'Total revenues'){ 
+                    let counter = {
+                        USD : 0,
+                        RUR : 0,
+                        BUR : 0,
+                    }
+                    VendorCard.find({}, (err, doc) => {
+                        if(err){console.log(err);}  
+                        for(let i = 0; i < doc.length; i++){
+                            if(doc[i].money == 'USD'){
+                                counter.USD += doc[i].cost;
+                            }
+                            if(doc[i].money == 'RUR'){
+                                counter.RUR += doc[i].cost;
+                            }
+                            if(doc[i].money == 'BUR'){
+                                counter.BUR += doc[i].cost;
+                            }
+                        }
+                        findingData[7] = counter;
+                        
+                        resolve();
+                    });
+                }else {resolve();} 
+            
+        });
+    }
+
+    task_0()                //Finding all registered vendors and return length of array
+    .then(() => task_1())   //Finding vendors in food groups
+    .then(() => task_2())   //Middle OSS value
+    .then(() => task_3())   //Quantity of 'red flags'
+    .then(() => task_4())   //Average of stars
+    .then(() => task_5())   //Hotline calls
+    .then(() => task_7())   //Total revenues    
+    .then(() => res.json(findingData));
 });
 
 
--- frontEnd/src/app/components/choose-category/report/report.component.ts
+++ frontEnd/src/app/components/choose-category/report/report.component.ts
@@ -4,6 +4,7 @@ import {FormControl} from '@angular/forms';
 import {MomentDateAdapter} from '@angular/material-moment-adapter';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 import * as _moment from 'moment';
+import * as jsPDF from 'jspdf';
 const moment = _moment;
 export const MY_FORMATS = {
   parse: {
@@ -48,6 +49,17 @@ export class ReportComponent implements OnInit {
   ];
   tasksToPush: {[k: string]: any} = {};
 
+  task_0 = '';
+  task_1 = '';
+  task_2 = '';
+  task_3 = '';
+  task_4 = '';
+  task_5 = '';
+  task_6 = '';
+  task_7 = '';
+
+  isShowReportBtn = false;
+
   check_checkBox(check_box, i){
     if(check_box.checked){
       this.tasksToPush[i] = this.taskList[i];      
@@ -63,10 +75,70 @@ export class ReportComponent implements OnInit {
         "Access-Control-Allow-Origin": "*"
       })
     };  
-    this.http.post('http://localhost:3000/report', this.tasksToPush, httpOptions).subscribe((data: any) => {
-        console.log(data); 
+    this.http.post('http://localhost:3000/report', this.tasksToPush, httpOptions).subscribe((data: any) => {        
+        this.isShowReportBtn = true;
+        if(data[0] !== ''){
+          this.task_0 = `Registered vendors: ${data[0]}` + '\r\n';
+        }
+        if(data[1] !== ''){
+          this.task_1 = `Vendors by food groups:
+                         Meat     - ${data[1].meat}
+                         Fastfood - ${data[1].fastfood}
+                         Fruit    - ${data[1].fruit} ` + '\r\n';
+        }
+        if(data[2] !== ''){
+          this.task_2 = `Average OSS: ${data[2]}` + '\r\n';
+        }
+        if(data[3] !== ''){
+          this.task_3 = `Quantity of red flags: ${data[3]}` + '\r\n';
+        }
+        if(data[4] !== ''){
+          this.task_4 = `Quantity of stars: ${data[4]}` + '\r\n';
+        }
+        if(data[5] !== ''){
+          this.task_5 = `Hotline calls: ${data[5]}` + '\r\n';
+        }
+        if(data[7] !== ''){
+          this.task_7 = `Total revenue:
+                         USD     - ${data[7].USD}
+                         BUR - ${data[7].BUR}
+                         RUR    - ${data[7].RUR} ` + '\r\n';
+        }
+       
     });
    
+  } 
+
+  report(dateVal){
+    let doc = new jsPDF();
+    doc.text(
+      `================================================
+       REPORT: ${dateVal}
+       ================================================
+      ` + '\r\n'
+      +
+      `${this.task_0}`
+      +
+      `${this.task_1}`
+      +
+      `${this.task_2}`
+      +
+      `${this.task_3}`
+      +
+      `${this.task_4}`
+      +
+      `${this.task_5}`
+      +
+      `${this.task_6}`
+      +
+      `${this.task_7}`
+      +
+      `================================================
+       created by HSFI service
+      `
+      , 10, 10);
+          
+      doc.save('test.pdf'); 
   }
 
 }
